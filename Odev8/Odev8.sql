f113285df98d1feb1b92f82a0079c2e8e9f6248e
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-10-08 00:52:09 +0300

    Added Homework8

---
+++ Odev8/Odev8.sql
@@ -0,0 +1,71 @@
+create database test;
+
+create table employee (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	birthday DATE NOT NULL,
+	email VARCHAR(50) NOT NULL
+);
+
+insert into employee (name, birthday, email) values ('Aloysius Hagstone', '2020-10-29', 'ahagstone0@admin.ch');
+insert into employee (name, birthday, email) values ('Chicky Aikin', '2021-01-16', 'caikin1@dedecms.com');
+insert into employee (name, birthday, email) values ('Jewelle Fernyhough', '2020-11-25', 'jfernyhough2@feedburner.com');
+insert into employee (name, birthday, email) values ('Uriel Hirtzmann', '2020-10-19', 'uhirtzmann3@jalbum.net');
+insert into employee (name, birthday, email) values ('Wernher Johannesson', '2020-11-18', 'wjohannesson4@goodreads.com');
+insert into employee (name, birthday, email) values ('Sherlock Sugarman', '2021-09-24', 'ssugarman5@upenn.edu');
+insert into employee (name, birthday, email) values ('Alon Wherrett', '2021-01-15', 'awherrett6@ftc.gov');
+insert into employee (name, birthday, email) values ('Simon McCluin', '2021-02-09', 'smccluin7@oaic.gov.au');
+insert into employee (name, birthday, email) values ('Salomi Sebrens', '2021-07-14', 'ssebrens8@github.com');
+insert into employee (name, birthday, email) values ('Cornell Outlaw', '2021-03-22', 'coutlaw9@house.gov');
+insert into employee (name, birthday, email) values ('Clarice Osman', '2021-07-02', 'cosmana@nydailynews.com');
+insert into employee (name, birthday, email) values ('Nolan Brockway', '2021-02-20', 'nbrockwayb@oaic.gov.au');
+insert into employee (name, birthday, email) values ('Barb Skupinski', '2021-05-16', 'bskupinskic@seattletimes.com');
+insert into employee (name, birthday, email) values ('Maurise Laurisch', '2021-03-24', 'mlaurischd@miibeian.gov.cn');
+insert into employee (name, birthday, email) values ('Wendie McAw', '2021-05-22', 'wmcawe@webnode.com');
+insert into employee (name, birthday, email) values ('Valery Alder', '2021-04-21', 'valderf@list-manage.com');
+insert into employee (name, birthday, email) values ('Renate Goodliffe', '2021-01-24', 'rgoodliffeg@wisc.edu');
+insert into employee (name, birthday, email) values ('Steffen Woolacott', '2021-07-08', 'swoolacotth@virginia.edu');
+insert into employee (name, birthday, email) values ('Jada Fogden', '2021-06-17', 'jfogdeni@nationalgeographic.com');
+insert into employee (name, birthday, email) values ('Shelley Collcott', '2021-05-26', 'scollcottj@surveymonkey.com');
+insert into employee (name, birthday, email) values ('Loretta Trank', '2021-01-12', 'ltrankk@cbslocal.com');
+insert into employee (name, birthday, email) values ('Barclay Grimsdike', '2021-09-05', 'bgrimsdikel@ucoz.ru');
+insert into employee (name, birthday, email) values ('Malinda Swaton', '2021-03-09', 'mswatonm@wordpress.org');
+insert into employee (name, birthday, email) values ('Chickie Charlotte', '2021-09-15', 'ccharlotten@usa.gov');
+insert into employee (name, birthday, email) values ('Maureen De Dantesie', '2020-11-01', 'mdeo@sakura.ne.jp');
+insert into employee (name, birthday, email) values ('Marion Ragbourne', '2021-07-08', 'mragbournep@wikispaces.com');
+insert into employee (name, birthday, email) values ('Lurette Anthoney', '2020-12-03', 'lanthoneyq@weather.com');
+insert into employee (name, birthday, email) values ('Therese Gotthard', '2021-06-12', 'tgotthardr@networksolutions.com');
+insert into employee (name, birthday, email) values ('Vernice Ketchell', '2021-07-02', 'vketchells@bloglovin.com');
+insert into employee (name, birthday, email) values ('Nixie Peinton', '2021-03-20', 'npeintont@theglobeandmail.com');
+insert into employee (name, birthday, email) values ('Evered Sherman', '2020-11-06', 'eshermanu@fastcompany.com');
+insert into employee (name, birthday, email) values ('Reinald Drinan', '2021-03-29', 'rdrinanv@last.fm');
+insert into employee (name, birthday, email) values ('Dex Brilleman', '2021-08-25', 'dbrillemanw@foxnews.com');
+insert into employee (name, birthday, email) values ('Horst Probet', '2020-12-25', 'hprobetx@google.es');
+insert into employee (name, birthday, email) values ('Kippie Swiers', '2021-08-18', 'kswiersy@eventbrite.com');
+insert into employee (name, birthday, email) values ('Shel Greenset', '2021-04-02', 'sgreensetz@auda.org.au');
+insert into employee (name, birthday, email) values ('Papagena Pile', '2020-10-27', 'ppile10@devhub.com');
+insert into employee (name, birthday, email) values ('Vasili Tuffs', '2020-11-24', 'vtuffs11@cbsnews.com');
+insert into employee (name, birthday, email) values ('Eleanor Persent', '2021-08-13', 'epersent12@epa.gov');
+insert into employee (name, birthday, email) values ('Shoshanna Myerscough', '2020-11-04', 'smyerscough13@flavors.me');
+insert into employee (name, birthday, email) values ('Chastity Boult', '2020-12-19', 'cboult14@github.io');
+insert into employee (name, birthday, email) values ('Winni Mac Geaney', '2020-12-02', 'wmac15@sfgate.com');
+insert into employee (name, birthday, email) values ('Heidie Anthes', '2021-07-06', 'hanthes16@bbc.co.uk');
+insert into employee (name, birthday, email) values ('Bryanty Slyne', '2020-12-20', 'bslyne17@over-blog.com');
+insert into employee (name, birthday, email) values ('Denyse Jorgesen', '2021-08-20', 'djorgesen18@go.com');
+insert into employee (name, birthday, email) values ('Bridgette Heintsch', '2021-02-28', 'bheintsch19@wikipedia.org');
+insert into employee (name, birthday, email) values ('Moritz Poynter', '2021-08-13', 'mpoynter1a@people.com.cn');
+insert into employee (name, birthday, email) values ('Hester Perchard', '2021-03-24', 'hperchard1b@goo.gl');
+insert into employee (name, birthday, email) values ('Helli Van Baaren', '2021-04-04', 'hvan1c@dot.gov');
+insert into employee (name, birthday, email) values ('Pernell Di Filippo', '2021-07-11', 'pdi1d@unc.edu');
+
+update employee set name = 'Evered Sherman';
+update employee set birthday = '1990-01-01';
+update employee set name = 'Hester Perchard' where id= 5;
+update employee set name = 'Pernell Di Filippo' where id= 40;
+update employee set email = 'hvan1c@dot.gov' where name = 'Winni Mac Geaney';
+
+delete from employee where id = 1;
+delete from employee where id = 40;
+delete from employee where name = 'Cornell Outlaw';
+delete from employee where birthday = '2021-02-28';
+delete from employee;
